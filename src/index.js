564c3f86587508143eb784742df6902e16858ddc
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2025-12-24 14:50:57 +0000

    Create returnWeather func and search bar

--- src/index.js
+++ src/index.js
@@ -2,15 +2,43 @@ import "./styles.css";
 
 let API_KEY = "BA3KZ8RM6CGG7T2P5MEVNX9KG";
 
+const form = document.querySelector("form");
+const searchBar = document.querySelector("#search");
+
 const fetchWeather = async function (location) {
   try {
     const res = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=uk&key=${API_KEY}&contentType=json`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=uk&elements=remove%3Acloudcover%2Cremove%3Adatetime%2Cremove%3AdatetimeEpoch%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Avisibility&key=${API_KEY}&contentType=json`,
     );
-    const weather = await res.json();
-    console.log(weather);
+    const json = await res.json();
+    returnWeather(json);
   } catch {
     console.log(Error);
   }
 };
+
+const returnWeather = async function (data) {
+  const {
+    resolvedAddress,
+    description,
+    currentConditions: { conditions, temp, feelslike, precip },
+  } = data;
+  const locationWeather = {
+    resolvedAddress,
+    description,
+    conditions,
+    temp,
+    feelslike,
+    precip,
+  };
+  console.log(locationWeather);
+  return locationWeather;
+};
+
+console.log(searchBar);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(searchBar.value);
+  fetchWeather(searchBar.value);
+});
 window.fetchWeather = fetchWeather;
