58ef5a147a8448706e39636c94101efd98f623e5
Matthew Ward <175315171+MatthewWard-Repos@users.noreply.github.com>
2026-02-10 15:55:50 +0000

    Reformat resultsBox.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import "./styles.css";
+import { createResultDivs, setUnits, createElement } from "./template.js";
 
 let API_KEY = "BA3KZ8RM6CGG7T2P5MEVNX9KG";
+let unitGroup = "uk";
 
-const form = document.querySelector("form");
+const form = document.querySelector("#form-weather");
 const searchBar = document.querySelector("#search");
 
 const fetchWeather = async function (location) {
   try {
     const res = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=uk&elements=remove%3Acloudcover%2Cremove%3Adatetime%2Cremove%3AdatetimeEpoch%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Avisibility&key=${API_KEY}&contentType=json`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&elements=remove%3Acloudcover%2Cremove%3Adatetime%2Cremove%3AdatetimeEpoch%2Cremove%3Adew%2Cremove%3Afeelslikemax%2Cremove%3Afeelslikemin%2Cremove%3Aprecipcover%2Cremove%3Apressure%2Cremove%3Asolarenergy%2Cremove%3Asolarradiation%2Cremove%3Avisibility&key=${API_KEY}&contentType=json`,
     );
     const json = await res.json();
     return returnWeather(json);
@@ -37,17 +39,18 @@ const returnWeather = async function (data) {
 };
 const displayWeather = function (weatherObj) {
   let resultsBox = document.getElementById("results-box");
-
-  Object.entries(weatherObj).forEach(([key, val]) => {
-    resultsBox.appendChild(createElement(key, val));
+  resultsBox.innerHTML = "";
+  resultsBox.style.display = "flex";
+  createResultDivs(resultsBox);
+  setUnits();
+  form.style.display = "none";
+  Object.entries(weatherObj).forEach(([key, val], index) => {
+    const child = resultsBox.childNodes[index];
+    if (!child) return;
+    let valCapitalised =
+      val.toString().slice(0, 1).toUpperCase() + val.toString().slice(1);
+    child.appendChild(createElement(key, valCapitalised));
   });
-};
-
-const createElement = function (className, content) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add(className);
-  newDiv.textContent = content;
-  return newDiv;
 };
 
 const listenSearch = function () {
@@ -57,5 +60,6 @@ const listenSearch = function () {
     displayWeather(weather);
   });
 };
+
 listenSearch();
 window.fetchWeather = fetchWeather;
